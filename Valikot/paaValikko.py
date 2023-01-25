c532a09565ccb9272d186d6f560f09b78958fd4f
Ei-Niin-Viisas <juvipe1@gmail.com>
2023-01-25 13:27:10 +0200

    Lisäsin koodiin kommentteja

--- Valikot/paaValikko.py
+++ Valikot/paaValikko.py
@@ -1,7 +1,12 @@
+import pygame
+
+#Luodaan luokka/olio päävalikolle
 class paaValikko:    
+    #olion konstruktori
     def __init__(self, testi:str):
         self.sana = testi
 
+    #olion testimetodi
     def tulosta(self):
         print(self.sana)
 
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame
 from Valikot.paaValikko import paaValikko
 
+#main-funktio
+#sisältää vain turhaa testikoodia
 def main():
     print("Toimii")
     sana:str = "tuloste"
@@ -8,5 +10,6 @@ def main():
     valikko.tulosta()
 
 
+#Kutsutaan main-funktiota
 if __name__ == "__main__":
     main()
