7e6835a4f9e66024246215d0f87ba23163b8954b
Ei-Niin-Viisas <juvipe1@gmail.com>
2023-02-20 14:38:52 +0200

    Git ignore tehty

--- testi2.py
+++ testi2.py
@@ -72,13 +72,6 @@ class platform(pygame.sprite.Sprite):
         self.surf.fill((255,0,0))
         self.rect = self.surf.get_rect(center = (WIDTH/2, HEIGHT - 10))
 
-class tausta(pygame.sprite.Sprite):
-    def __init__(self):
-        super().__init__()
-        self.surf = pygame.Surface((WIDTH, HEIGHT))
-        self.surf.fill((0,0,0))
-        self.rect = self.surf.get_rect()
-
 
 
 PT1 = platform()
