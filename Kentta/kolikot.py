2ea29a18f99ebd9741a4647ab3157a4a8751f5eb
eduzii <eetuauvinen@hotmail.com>
2023-04-03 12:53:51 +0300

    jeejee

--- Kentta/kolikot.py
+++ Kentta/kolikot.py
@@ -1,16 +1,21 @@
 import pygame, sys
 import pygame
+from Kentta.kentta import platform
+import random
 
 
 class Kolikko(pygame.sprite.Sprite):
-    def __init__(self, x, y):
+    def __init__(self, width, height):
         super().__init__()
         self.image = pygame.image.load("pics/coin.png")
         self.rect = self.image.get_rect()
-        self.rect.x = x
-        self.rect.y = y
+        self.rect.x = platform(width(200))
+        self.rect.y = platform(height(150))
+        
 
-    def collision(self, player):
+    """def collision(self, player):
         if pygame.sprite.collide_rect(self, player):
             self.kill()
-            player.score += 10
+            player.score += 10"""
+
+
