121ef1f60327f6936f53da68816e074d742819cd
eeelkku <102227402+eeelkku@users.noreply.github.com>
2023-04-17 12:24:04 +0300

    Update game_data.py

--- game_data.py
+++ game_data.py
@@ -9,3 +9,23 @@ level_0 = {
     'constraints': 'Kentta/levels/0/level_0_constraints.csv',
     'player': 'Kentta/levels/0/level_0_player.csv',
     'grass': 'Kentta/levels/0/level_0_grass.csv'}
+level_1 = {
+    'terrain': 'Kentta/levels/1/level_1_terrain.csv',
+    'coins': 'Kentta/levels/1/level_1_coins.csv',
+    'fg palms': 'Kentta/levels/1/level_1_fg_palms.csv',
+    'bg palms': 'Kentta/levels/1/level_1_bg_palms.csv',
+    'crates': 'Kentta/levels/1/level_1_crates.csv',
+    'enemies': 'Kentta/levels/1/level_1_enemies.csv',
+    'constraints': 'Kentta/levels/1/level_1_constraints.csv',
+    'player': 'Kentta/levels/1/level_1_player.csv',
+    'grass': 'Kentta/levels/1/level_1_grass.csv'}
+level_2 {
+    'terrain': 'Kentta/levels/2/level_2_terrain.csv',
+    'coins': 'Kentta/levels/2/level_2_coins.csv',
+    'fg palms': 'Kentta/levels/2/level_2_fg_palms.csv',
+    'bg palms': 'Kentta/levels/2/level_2_bg_palms.csv',
+    'crates': 'Kentta/levels/2/level_2_crates.csv',
+    'enemies': 'Kentta/levels/2/level_2_enemies.csv',
+    'constraints': 'Kentta/levels/2/level_2_constraints.csv',
+    'player': 'Kentta/levels/2/level_2_player.csv',
+    'grass': 'Kentta/levels/2/level_2_grass.csv'}
